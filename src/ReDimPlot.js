895d6e5bfae367d4af1f75f684839fccafc619bb
redpanda-cat <leungs1@mskcc.org>
2019-04-01 15:50:59 -0400

    Add basic reDim plot

--- src/ReDimPlot.js
+++ src/ReDimPlot.js
@@ -1,5 +1,53 @@
 import React from "react";
 
-const ReDimPlot = ({ sampleID }) => <span>{sampleID}</span>;
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+import XYFrame from "semiotic/lib/XYFrame";
+
+const QUERY = gql`
+  query($sampleID: String!) {
+    cells(sampleID: $sampleID) {
+      id
+      x
+      y
+      cluster
+      celltype
+    }
+  }
+`;
+
+const ReDimPlot = ({ sampleID }) =>
+  !sampleID ? null : (
+    <Query query={QUERY} variables={{ sampleID }}>
+      {({ loading, error, data }) => {
+        if (loading) return null;
+        if (error) return null;
+
+        const frameProps = getFrameProps(data.cells);
+        return <XYFrame {...frameProps} />;
+      }}
+    </Query>
+  );
+
+const getFrameProps = cells => ({
+  points: cells,
+
+  size: [700, 500],
+  margin: { left: 60, bottom: 90, right: 10, top: 40 },
+
+  xAccessor: "x",
+  yAccessor: "y",
+
+  pointStyle: d => ({
+    r: 3,
+    fill: "#9fd0cb",
+    stroke: "white"
+  }),
+  axes: [
+    { orient: "left", label: "y" },
+    { orient: "bottom", label: { name: "x", locationDistance: 55 } }
+  ]
+});
 
 export default ReDimPlot;
