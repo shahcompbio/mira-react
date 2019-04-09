81414b1d0f03cc8864856e676ccad706cc1031da
redpanda-cat <leungs1@mskcc.org>
2019-04-09 17:21:18 -0400

    Update color label selection for long lists (WIP)

--- src/ReDim/LabelSelect.js
+++ src/ReDim/LabelSelect.js
@@ -1,10 +1,60 @@
 import React, { Component } from "react";
 import { Dropdown } from "semantic-ui-react";
 
+import Autocomplete from "react-autocomplete";
+import { List, CellMeasurer, CellMeasurerCache } from "react-virtualized";
+
 class LabelSelect extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchingFor: "",
+      selection: ""
+    };
+  }
   componentDidMount() {
     this.props.onSelect(this.props.data[0]);
   }
+
+  renderItem = item => {
+    return <div>{item.title}</div>;
+  };
+  renderMenu = (items, searchingFor, autocompleteStyle) => {
+    const rowRenderer = ({ key, index, parent, style }) => {
+      const Item = items[index];
+      const onMouseDown = e => {
+        if (e.button === 0) {
+          Item.props.onClick(e);
+        }
+      };
+      return (
+        <div key={key} style={style}>
+          {Item}
+        </div>
+      );
+    };
+
+    return (
+      <List
+        rowHeight={20}
+        height={500}
+        rowCount={items.length}
+        rowRenderer={rowRenderer}
+        width={200}
+        style={{
+          position: "absolute",
+          backgroundColor: "white",
+          border: "1px solid black",
+          height: "auto",
+          maxHeight: "500px",
+          overflowY: "scroll",
+          display: items.length ? "block" : "none"
+        }}
+      />
+    );
+  };
+
   render() {
     const { data, onSelect } = this.props;
 
@@ -18,17 +68,26 @@ class LabelSelect extends Component {
       text: label.title,
       value: label.id
     }));
-
+    console.log(data);
     return (
-      <span>
-        Color by &nbsp;
-        <Dropdown
-          onChange={handleChange}
-          selection
-          options={options}
-          defaultValue={options[0].value}
-        />
-      </span>
+      <Autocomplete
+        renderItem={this.renderItem}
+        items={data}
+        getItemValue={item => item.title}
+        value={this.state.searchingFor}
+        onChange={(e, value) => this.setState({ searchingFor: value })}
+        onSelect={onSelect}
+        renderMenu={this.renderMenu}
+      />
+      // <span>
+      //   Color by &nbsp;
+      //   <Dropdown
+      //     onChange={handleChange}
+      //     selection
+      //     options={options}
+      //     defaultValue={options[0].value}
+      //   />
+      // </span>
     );
   }
 }
