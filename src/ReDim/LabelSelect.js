2edff1270fb63062608b218d248cdae0538aa17d
redpanda-cat <leungs1@mskcc.org>
2019-04-10 14:51:10 -0400

    Support for drop down of large lists

--- src/ReDim/LabelSelect.js
+++ src/ReDim/LabelSelect.js
@@ -1,94 +1,60 @@
 import React, { Component } from "react";
-import { Dropdown } from "semantic-ui-react";
 
-import Autocomplete from "react-autocomplete";
-import { List, CellMeasurer, CellMeasurerCache } from "react-virtualized";
+import Select from "react-select";
+import { List } from "react-virtualized";
 
 class LabelSelect extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      searchingFor: "",
-      selection: ""
-    };
-  }
   componentDidMount() {
     this.props.onSelect(this.props.data[0]);
   }
 
-  renderItem = item => {
-    return <div>{item.title}</div>;
-  };
-  renderMenu = (items, searchingFor, autocompleteStyle) => {
-    const rowRenderer = ({ key, index, parent, style }) => {
-      const Item = items[index];
-      const onMouseDown = e => {
-        if (e.button === 0) {
-          Item.props.onClick(e);
-        }
-      };
-      return (
-        <div key={key} style={style}>
-          {Item}
-        </div>
-      );
-    };
-
-    return (
-      <List
-        rowHeight={20}
-        height={500}
-        rowCount={items.length}
-        rowRenderer={rowRenderer}
-        width={200}
-        style={{
-          position: "absolute",
-          backgroundColor: "white",
-          border: "1px solid black",
-          height: "auto",
-          maxHeight: "500px",
-          overflowY: "scroll",
-          display: items.length ? "block" : "none"
-        }}
-      />
-    );
-  };
-
   render() {
     const { data, onSelect } = this.props;
 
-    const handleChange = (e, { value }) => {
-      const label = data.filter(label => label.id === value);
-      return onSelect(label[0]);
+    const handleChange = item => {
+      onSelect({
+        id: item.value,
+        title: item.label
+      });
     };
-
     const options = data.map(label => ({
-      key: label.id,
-      text: label.title,
-      value: label.id
+      value: label.id,
+      label: label.title
     }));
-    console.log(data);
+
     return (
-      <Autocomplete
-        renderItem={this.renderItem}
-        items={data}
-        getItemValue={item => item.title}
-        value={this.state.searchingFor}
-        onChange={(e, value) => this.setState({ searchingFor: value })}
-        onSelect={onSelect}
-        renderMenu={this.renderMenu}
-      />
-      // <span>
-      //   Color by &nbsp;
-      //   <Dropdown
-      //     onChange={handleChange}
-      //     selection
-      //     options={options}
-      //     defaultValue={options[0].value}
-      //   />
-      // </span>
+      <div style={{ width: "300px" }}>
+        <span>
+          Color By
+          <Select
+            defaultValue={options[0]}
+            onChange={handleChange}
+            options={options}
+            components={{ MenuList }}
+          />
+        </span>
+      </div>
     );
   }
 }
+
+const MenuList = props => {
+  const rowRenderer = ({ key, index, isScrolling, isVisible, style }) => (
+    <div key={key}>{props.children[index]}</div>
+  );
+
+  return (
+    <List
+      rowHeight={20}
+      height={500}
+      width={300}
+      rowCount={props.options.length}
+      rowRenderer={rowRenderer}
+      style={{
+        width: "100%",
+        zIndex: 11
+      }}
+    />
+  );
+};
 export default LabelSelect;
