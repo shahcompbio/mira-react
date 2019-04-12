dce0f37f439341cf726ff40dc54b9e41175e01a2
redpanda-cat <leungs1@mskcc.org>
2019-04-12 16:28:29 -0400

    Refactor Abundance plot

--- src/ReDim/AbundancePlot.js
+++ src/ReDim/AbundancePlot.js
@@ -8,20 +8,44 @@ import { curveCardinal } from "d3-shape";
 import { getColorGradient } from "./colors";
 
 const AbundancePlot = ({ data, colorScale, hoverBehavior, label }) => {
-  if (label.type === "categorical") {
-    const frameProps = getFrameProps(data, colorScale, label.title);
-    return <OrdinalFrame {...frameProps} customHoverBehavior={hoverBehavior} />;
-  } else {
-    const frameProps = getFramePropsLine(data, colorScale);
-    return <XYFrame {...frameProps} customHoverBehavior={hoverBehavior} />;
-  }
+  const frameProps = getFrameProps(data, colorScale, label);
+
+  return label.type === "categorical" ? (
+    <OrdinalFrame {...frameProps} customHoverBehavior={hoverBehavior} />
+  ) : (
+    <XYFrame {...frameProps} customHoverBehavior={hoverBehavior} />
+  );
 };
 
-const getFramePropsLine = (data, colorScale) => ({
-  lines: [{ coordinates: data }],
+const getFrameProps = (data, colorScale, label) => {
+  const defaultFrameProps = {
+    size: [300, 300],
+    margin: 70,
 
-  size: [300, 300],
-  margin: 70,
+    hoverAnnotation: true,
+
+    title: `${label.title} Abundances`
+  };
+
+  const tooltipContent = d => (
+    <div className="tooltip-content">
+      <p>
+        <b>{d.name}</b>
+      </p>
+      <p>{d.count} cells</p>
+    </div>
+  );
+
+  const additionalFrameProps =
+    label.type === "categorical"
+      ? getFramePropsBar(data, colorScale, tooltipContent)
+      : getFramePropsLine(data, colorScale, tooltipContent);
+
+  return { ...defaultFrameProps, ...additionalFrameProps };
+};
+
+const getFramePropsLine = (data, colorScale, tooltipContent) => ({
+  lines: [{ coordinates: data }],
 
   xAccessor: "name",
   yAccessor: "count",
@@ -44,43 +68,21 @@ const getFramePropsLine = (data, colorScale) => ({
   ],
   showLinePoints: "top",
 
-  hoverAnnotation: true,
   additionalDefs: getColorGradient(data.map(labelValue => labelValue.name)),
-  tooltipContent: d => {
-    return (
-      <div className="tooltip-content">
-        <p>ID: {d.name}</p>
-        <p>Count: {d.count}</p>
-      </div>
-    );
-  }
+  tooltipContent
 });
 
-const getFrameProps = (data, colorScale, title) => {
-  return {
-    data: data,
+const getFramePropsBar = (data, colorScale, tooltipContent) => ({
+  data: data,
 
-    size: [300, 300],
-    margin: 70,
+  type: "bar",
+  rAccessor: "count",
+  rScaleType: scalePow().exponent(0.7),
+  oAccessor: "name",
+  axes: [{ orient: "left", label: "# Cells" }],
+  style: d => ({ fill: colorScale(d.name), stroke: "white" }),
 
-    type: "bar",
-    rAccessor: "count",
-    rScaleType: scalePow().exponent(0.7),
-    oAccessor: "name",
-    axes: [{ orient: "left", label: " " }],
-    style: d => ({ fill: colorScale(d.name), stroke: "white" }),
-
-    title: `${title} Abundances`,
-    hoverAnnotation: true,
-    tooltipContent: ({ pieces }) => {
-      return (
-        <div className="tooltip-content">
-          <p>ID: {pieces[0].name}</p>
-          <p>Count: {pieces[0].count}</p>
-        </div>
-      );
-    }
-  };
-};
+  tooltipContent: ({ pieces }) => tooltipContent(pieces[0])
+});
 
 export default AbundancePlot;
