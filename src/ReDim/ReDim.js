7c142251946e598fdab054b179627396e0b5bb84
vbojilova <viki.bojilova@gmail.com>
2019-06-06 14:53:12 +0000

    Moved colour by drop down out of re dim and into the select folder

--- src/ReDim/ReDim.js
+++ src/ReDim/ReDim.js
@@ -1,80 +1,27 @@
-import React, {Component} from "react";
-import {Segment, Divider} from "semantic-ui-react";
-import {Query} from "react-apollo";
-import gql from "graphql-tag";
+import React from "react";
 
-import LabelSelect from "./LabelSelect";
+import Grid from "@material-ui/core/Grid";
 import Content from "./Content";
 
-const QUERY = gql`
-  query($patientID: String!, $sampleID: String!) {
-    colorLabels(patientID: $patientID, sampleID: $sampleID) {
-      id
-      title
-      labels {
-        id
-        title
-        type
-      }
-    }
-  }
-`;
-
-class ReDim extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      label: null
-    };
-  }
-
-  onLabelSelect = label => {
-    this.setState(() => ({label}));
-  };
-
-  render() {
-    const {patientID, sampleID} = this.props;
-
-    return !sampleID ? null : (
-      <Query query={QUERY} variables={{patientID, sampleID}}>
-        {({loading, error, data}) => {
-          if (loading) return null;
-          if (error) return null;
-
-          return (
-            <div style={DivStyles}>
-              <Segment vertical>
-                <Content
-                  patientID={patientID}
-                  sampleID={sampleID}
-                  label={this.state.label}
-                />
-              </Segment>{" "}
-              <Segment
-                vertical
-                style={{width: "25%", marginTop: "50px", marginLeft: "-20vw"}}
-              >
-                <LabelSelect
-                  data={data.colorLabels}
-                  onSelect={this.onLabelSelect}
-                />
-                <Divider style={{margin: "25px"}} />
-              </Segment>
-            </div>
-          );
-        }}
-      </Query>
-    );
-  }
-}
-
+const ReDim = ({patientID, sampleID, screenHeight, screenWidth, label}) => {
+  return !label ? null : (
+    <div style={DivStyles}>
+      <Grid item>
+        <Content
+          screenHeight={screenHeight}
+          screenWidth={screenWidth}
+          patientID={patientID}
+          sampleID={sampleID}
+          label={label}
+        />
+      </Grid>{" "}
+    </div>
+  );
+};
 const DivStyles = {
   width: "70%",
   display: "flex",
-  flexDirection: "row",
-
-  backgroundColor: ""
+  flexDirection: "row"
 };
 
 export default ReDim;
---
+++ src/Select/LabelSelect.js
@@ -0,0 +1,231 @@
+import React, {Component} from "react";
+
+import Select, {createFilter} from "react-select";
+import {FixedSizeList as List} from "react-window";
+
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import FormControl from "@material-ui/core/FormControl";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import TextField from "@material-ui/core/TextField";
+import ArrowDropDown from "@material-ui/icons/ArrowDropDown";
+
+import {withStyles} from "@material-ui/styles";
+
+const groupBadgeStyles = {
+  backgroundColor: "#CCCCCC",
+  borderRadius: "2em",
+  color: "#172B4D",
+  display: "inline-block",
+  float: "right",
+  fontSize: 12,
+  fontWeight: "normal",
+  lineHeight: "1",
+  minWidth: 1,
+  padding: "0.16666666666667em 0.5em",
+  textAlign: "center"
+};
+
+const styles = {
+  root: {
+    width: 200,
+    flexGrow: 1,
+    height: 250
+  },
+  textField: {paddingLeft: "-15px", width: 200, marginLeft: "70px"},
+  input: {
+    display: "flex",
+    padding: 5,
+    height: "auto"
+  },
+  valueContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    flex: 1,
+    alignItems: "center",
+    overflow: "hidden"
+  },
+  singleValue: {
+    fontSize: 16
+  },
+  paper: {
+    position: "absolute",
+    zIndex: 1,
+    left: 0,
+    right: 0
+  }
+};
+class LabelSelect extends Component {
+  componentDidMount() {
+    this.props.onSelect(this.props.data[0]["labels"][0]);
+  }
+
+  render() {
+    const {data, onSelect, classes} = this.props;
+
+    const allOptions = data.reduce(
+      (options, group) => [...options, ...group.labels],
+      []
+    );
+
+    const handleChange = item => {
+      if (item) {
+        const result = allOptions.filter(datum => datum.id === item.value)[0];
+        onSelect(result);
+      } else {
+        onSelect(null);
+      }
+    };
+
+    const groupOptions = data.map(group => ({
+      label: group.title,
+      options: group.labels.map(label => ({
+        value: label.id,
+        label: label.title
+      }))
+    }));
+
+    return (
+      <Select
+        classes={classes}
+        defaultValue={groupOptions[0]["options"][0]}
+        onChange={handleChange}
+        options={groupOptions}
+        TextFieldProps={{
+          className: classes.textField,
+          label: "Colour by:",
+          margin: "normal",
+          InputLabelProps: {
+            shrink: true,
+            className: classes.textFieldLabel,
+            htmlFor: "react-select-single"
+          },
+          placeholder: "Colour by"
+        }}
+        components={{
+          Control,
+          MenuList,
+          Group,
+          ValueContainer,
+          Menu,
+          Option,
+          DropdownIndicator,
+          IndicatorSeparator
+        }}
+        isSearchable
+        filterOption={createFilter({ignoreAccents: false})}
+      />
+    );
+  }
+}
+const Option = props => (
+  <MenuItem
+    ref={props.innerRef}
+    selected={props.isFocused}
+    component="span"
+    style={{
+      fontWeight: props.isSelected ? 500 : 400,
+      background: props.isSelected ? "#e8ecf1" : ""
+    }}
+    {...props.innerProps}
+  >
+    {props.children}
+  </MenuItem>
+);
+const DropdownIndicator = props => <ArrowDropDown />;
+const Menu = props => (
+  <Paper
+    square
+    className={props.selectProps.classes.paper}
+    {...props.innerProps}
+  >
+    {props.children}
+  </Paper>
+);
+const ValueContainer = props => (
+  <div
+    style={{width: 200}}
+    className={props.selectProps.classes.valueContainer}
+  >
+    {props.children}
+  </div>
+);
+const inputComponent = ({inputRef, ...props}) => (
+  <span ref={inputRef} {...props} />
+);
+const Control = props => {
+  const {
+    children,
+    innerProps,
+    innerRef,
+    selectProps: {classes, TextFieldProps}
+  } = props;
+  return (
+    <FormControl
+      style={{
+        width: 200,
+        paddingLeft: 15,
+        paddingRight: 15,
+        display: "flex",
+        flexWrap: "wrap"
+      }}
+    >
+      <TextField
+        InputProps={{
+          inputComponent,
+          inputProps: {
+            className: classes.input,
+            ref: innerRef,
+            children,
+            ...innerProps
+          }
+        }}
+        {...TextFieldProps}
+      />
+    </FormControl>
+  );
+};
+const Group = ({label, options}) => (
+  <div
+    style={{
+      background: "#8799af",
+      padding: "14px",
+      fontWeight: "bold",
+      margin: "0px 0px !important"
+    }}
+  >
+    <Typography component="span">{label}</Typography>
+    <span style={groupBadgeStyles}>{options.length}</span>
+  </div>
+);
+const IndicatorSeparator = () => null;
+const MenuList = ({children, maxHeight}) => {
+  const height = 48;
+
+  const options = Array.isArray(children)
+    ? children.reduce(
+        (options, child) => [...options, child, ...child.props.children],
+        []
+      )
+    : [];
+  return (
+    <List
+      height={maxHeight + 300}
+      width={"225px"}
+      itemCount={options.length}
+      itemSize={height}
+    >
+      {({index, style}) => {
+        return (
+          <ListItem style={{...style, padding: "0px", margin: 0}} key={index}>
+            <ListItemText primary={options[index]} />
+          </ListItem>
+        );
+      }}
+    </List>
+  );
+};
+
+export default withStyles(styles)(LabelSelect);
