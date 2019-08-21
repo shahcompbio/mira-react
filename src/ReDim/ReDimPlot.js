78dc7d8634af90d089b871b7fa83ec06a6b1334e
Charumathi Badrinath <charumathi.badrinath@gmail.com>
2019-08-21 16:07:12 -0400

    Fixed mistake

--- src/ReDim/ReDimPlot.js
+++ src/ReDim/ReDimPlot.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import XYFrame from "semiotic/lib/XYFrame";
 
 const ReDimPlot = ({
@@ -68,17 +68,15 @@ const ReDimPlot = ({
     );
   };
 
-  const frameProps = (
-    <GetFrameProps
-      data={data}
-      colorScale={colorScale}
-      labelTitle={labelTitle}
-      height={height}
-      wid={width}
-      title={title}
-      getMostAbundant={getMostAbundant}
-      highlightedCells
-    />
+  const frameProps = getFrameProps(
+    data,
+    colorScale,
+    labelTitle,
+    height,
+    width,
+    title,
+    getMostAbundant,
+    highlightedCells
   );
 
   return (
@@ -89,82 +87,71 @@ const ReDimPlot = ({
           {title === labelTitle && title !== "Site" ? " Expression" : ""}
         </h3>
       </center>
-      <GetFrameProps
-        data={data}
-        colorScale={colorScale}
-        labelTitle={labelTitle}
-        height={height}
-        width={width}
-        title={title}
-        getMostAbundant={getMostAbundant}
-        highlightedCells={highlightedCells}
-      />
+      <XYFrame {...frameProps} />
     </div>
   );
 };
 
-class GetFrameProps extends Component {
-  render() {
-    const {
-      data,
-      colorScale,
-      labelTitle,
-      height,
-      width,
-      title,
-      getMostAbundant,
-      highlightedCells
-    } = this.props;
+const getFrameProps = (
+  data,
+  colorScale,
+  labelTitle,
+  height,
+  width,
+  title,
+  getMostAbundant,
+  highlightedCells
+) => ({
+  summaries: data,
+
+  points: highlightedCells,
+
+  size: [width, height],
+  margin: { left: 25, bottom: 90, right: 10, top: 10 },
+
+  summaryType: {
+    type: "hexbin",
+    bins: 0.04
+  },
+
+  xAccessor: "x",
+  yAccessor: "y",
+
+  canvasPoints: true,
+
+  canvasAreas: true,
+
+  summaryStyle: d => ({
+    fill: colorScale(getMostAbundant(d.data)),
+    fillOpacity: 0.3
+  }),
+
+  pointStyle: d => ({
+    r: 4,
+    fill: colorScale(
+      title === "Cell Types" ? d.celltype : title === "Site" ? d.site : d.label
+    )
+  }),
+  axes: [
+    { orient: "left", label: " " },
+    { orient: "bottom", label: { name: " ", locationDistance: 55 } }
+  ],
+  hoverAnnotation: true,
+
+  tooltipContent: d => {
     return (
-      <XYFrame
-        summaries={data}
-        points={highlightedCells}
-        size={[width, height]}
-        margin={{ left: 25, bottom: 90, right: 10, top: 10 }}
-        summaryType={{
-          type: "hexbin",
-          bins: 0.04
-        }}
-        xAccessor={"x"}
-        yAccessor={"y"}
-        canvasPoints={true}
-        canvasAreas={true}
-        summaryStyle={d => ({
-          fill: colorScale(getMostAbundant(d.data)),
-          fillOpacity: 0.3
-        })}
-        pointStyle={d => ({
-          r: 4,
-          fill: colorScale(
-            title === "Cell Types"
-              ? d.celltype
-              : title === "Site"
-              ? d.site
-              : d.label
-          )
-        })}
-        axes={[
-          { orient: "left", label: " " },
-          { orient: "bottom", label: { name: " ", locationDistance: 55 } }
-        ]}
-        hoverAnnotation={true}
-        tooltipContent={d => {
-          return (
-            <div className="tooltip-content">
-              <p>
-                {title === "Cell Types" ? "Cell Type" : labelTitle}:{" "}
-                {title === "Cell Types"
-                  ? d.celltype
-                  : title === "Site"
-                  ? d.site
-                  : d.label}
-              </p>
-            </div>
-          );
-        }}
-      />
+      <div className="tooltip-content">
+        <p>
+          {title === "Cell Types" ? "Cell Type" : labelTitle}:{" "}
+          {title === "Cell Types"
+            ? d.celltype
+            : title === "Site"
+            ? d.site
+            : d.label}
+        </p>
+      </div>
     );
   }
-}
+});
 
 export default ReDimPlot;
