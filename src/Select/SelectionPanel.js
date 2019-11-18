ebc6a333685b199a0ecc24f7089f045d181a64bf
redpanda-cat <leungs1@mskcc.org>
2019-11-18 10:44:23 -0800

    Clean up old files

--- src/Select/SelectionPanel.js
+++ src/Select/SelectionPanel.js
@@ -19,16 +19,6 @@ const QUERY = gql`
   }
 `;
 
-// const styles = {
-//   paper: {
-//     width: "100%",
-//     padding: "10px",
-//     marginBottom: "30px",
-//     boxShadow:
-//       "0px 1px 13px 0px rgba(174, 181, 177, 0.18), -3px 1px 14px 2px rgba(129, 133, 136, 0.17), 0px 1px 8px -1px rgba(78, 79, 80, 0.23);"
-//   }
-// };
-
 const SelectionPanel = ({ classes }) => {
   let history = useHistory();
   const location = useLocation();
