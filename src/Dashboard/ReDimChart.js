180ecf449704679904ca5f634c1e59fe0cb7561e
redpanda-cat <leungs1@mskcc.org>
2020-02-07 15:06:59 -0500

    alter color of empty subsetted bins

--- src/Dashboard/ReDimChart.js
+++ src/Dashboard/ReDimChart.js
@@ -194,7 +194,7 @@ const getFrameProps = ({
     fill: getColor(d, label, highlightedGroup, colorScale),
     fillOpacity:
       highlightedGroup && highlightedGroup["label"] === label["label"]
-        ? Math.max(d["value"], 0.5)
+        ? Math.max(d["value"], 0.05)
         : 1
   }),
 
@@ -220,191 +220,14 @@ const getColor = (d, label, highlightedGroup, colorScale) => {
   if (!highlightedGroup) {
     // nothing is highlighted, return original color
     return colorScale(d["value"]);
-  } else if (d["value"] === 0) {
+  } else if (d["value"] === "") {
     // 0 if highlighted group doesn't overlap; so grey out
 
-    return "#ccc";
+    return "#eee";
   } else if (highlightedGroup["label"] === label["label"]) {
     return colorScale(d["label"]);
   } else {
     return colorScale(d["value"]);
-  }
-};
-
-// const getQuery = label =>
-//   label["label"] === "celltype" ? QUERY_CELLTYPES : QUERY_OTHER;
-
-// const ReDimChart = ({
-//   labels,
-//   index,
-//   onSelect,
-//   onLegendHover,
-//   highlightedGroup,
-//   width
-// }) => {
-//   const location = useLocation();
-//   const [dashboardType, dashboardID] = [
-//     useDashboardType(location),
-//     useDashboardID(location)
-//   ];
-//   const { data, loading } = useQuery(getQuery(labels[index]), {
-//     variables: {
-//       dashboardType,
-//       dashboardID,
-//       props: labels,
-//       label: labels[index]
-//     }
-//   });
-
-//   // This is messy, but this is to store previous label state (and we only want that to update when loading is done)
-//   const prevLabelRef = useRef();
-//   const prevIDRef = useRef();
-//   useEffect(() => {
-//     if (!loading) {
-//       prevLabelRef.current = labels[index]["label"];
-//       prevIDRef.current = dashboardID;
-//     }
-//   }, [loading, labels[index]["label"]]);
-
-//   const prevLabel = prevLabelRef.current;
-//   const prevID = prevIDRef.current;
-
-//   // only want spinny loading if it's initial load (no previous data) for new data set OR this particular label has been changed
-
-//   if (
-//     (loading && prevID !== dashboardID) ||
-//     !data ||
-//     (loading && prevLabel !== labels[index]["label"])
-//   ) {
-//     return (
-//       <BaseChart onSelect={onSelect} label={labels[index]}>
-//         <CircularProgress />
-//       </BaseChart>
-//     );
-//   }
-
-//   const { cells, celltypes, dashboardAttributeValues } = data;
-//   const cellProps = cells.map(cell => {
-//     const cellProps = cell["values"].reduce(
-//       (props, value) => ({ ...props, [value["label"]]: value["value"] }),
-//       {}
-//     );
-
-//     return { ...cell, ...cellProps };
-//   });
-
-//   const colorData = celltypes
-//     ? celltypes.map(celltype => celltype["name"])
-//     : dashboardAttributeValues;
-
-//   const colorScale = getColorScale(labels[index], colorData);
-//   return (
-//     <BaseChart onSelect={onSelect} label={labels[index]}>
-//       <Grid item>
-//         <XYFrame
-//           {...getFrameProps({
-//             data: cellProps,
-//             label: labels[index],
-//             highlightedGroup,
-//             colorScale,
-//             width
-//           })}
-//         />
-//       </Grid>
-//       <Grid item>
-//         <Legend
-//           data={colorData}
-//           colorScale={colorScale}
-//           width={width}
-//           label={labels[index]}
-//           onHover={onLegendHover}
-//         />
-//       </Grid>
-//     </BaseChart>
-//   );
-// };
-
-const isInRange = (point, min) =>
-  min <= point && point < min + (min < 1 ? 0.1 : 1);
-
-// const getFrameProps = ({
-//   data,
-//   label,
-//   highlightedGroup,
-//   colorScale,
-//   width
-// }) => ({
-//   summaries: data,
-//   points: highlightedGroup
-//     ? data.filter(datum =>
-//         highlightedGroup["label"] === "celltype"
-//           ? datum[highlightedGroup["label"]] === highlightedGroup["value"]
-//           : isInRange(
-//               datum[highlightedGroup["label"]],
-//               highlightedGroup["value"]
-//             )
-//       )
-//     : [],
-
-//   size: [width, 500],
-//   margin: { left: 25, bottom: 45, right: 25, top: 0 },
-
-//   xAccessor: "x",
-//   yAccessor: "y",
-//   summaryType: {
-//     type: "hexbin",
-//     bins: 0.03,
-//     binValue: d => getMaxLabel(d, label)
-//   },
-//   canvasAreas: true,
-//   summaryStyle: d => ({
-//     fill: d["value"] === "" ? "#FFFFFF" : colorScale(d["value"]),
-//     fillOpacity: 0.3
-//   }),
-//   canvasPoints: true,
-//   pointStyle: d => {
-//     return {
-//       r: 4,
-//       fill: colorScale(d[label["label"]]),
-//       stroke: "#eee",
-//       strokeWidth: 1
-//     };
-//   },
-
-//   axes: [
-//     { orient: "left", label: " " },
-//     { orient: "bottom", label: { name: " ", locationDistance: 55 } }
-//   ]
-// });
-
-const getMaxLabel = (data, label) => {
-  if (label["label"] === "celltype" || label["type"] === "SAMPLE") {
-    const counts = data
-      .map(point => point[label["label"]])
-      .reduce(
-        (countMap, point) =>
-          countMap.hasOwnProperty(point)
-            ? { ...countMap, [point]: countMap[point] + 1 }
-            : { ...countMap, [point]: 1 },
-        {}
-      );
-
-    const celltypes = Object.keys(counts);
-
-    return celltypes.length === 0
-      ? ""
-      : celltypes.reduce(
-          (currMax, key) => (counts[key] > counts[currMax] ? key : currMax),
-          Object.keys(counts)[0]
-        );
-  } else {
-    const total = data
-      .map(point => point[label["label"]])
-      .reduce((currSum, point) => currSum + point, 0);
-
-    const count = data.length;
-
-    return total / count;
   }
 };
 
