564678de17adb05bcad6bedacac8f9b859975154
redpanda-cat <leungs1@mskcc.org>
2020-02-12 14:16:28 -0500

    Add text box

--- src/sandbox/CumulativeGenePlot.js
+++ src/sandbox/CumulativeGenePlot.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import XYFrame from "semiotic/lib/XYFrame";
 
 import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import { useQuery } from "@apollo/react-hooks";
 
 import Correlation from "./Correlation";
@@ -12,9 +14,7 @@ const TEST_DASHBOARDTYPE = "patient";
 
 const TEST_ID = "SPECTRUM-OV-014_CD45P";
 const TEST_LABEL = { label: "celltype", type: "CELL" };
-const GENES = ["CD2", "CD4", "VIM", "CAV1", "PTPRC"]
-
-
+const GENES = ["CD2", "CD4", "VIM", "CAV1", "PTPRC"];
 
 const QUERY_CUMULATIVE_GENES = gql`
   query($dashboardID: String!, $genes: [String!]!) {
@@ -37,29 +37,55 @@ const getCumulativeGenes = (ID, GENES) => {
   if (loading && !data) {
     return null;
   }
-  console.log(data)
-  return data.cumulativeGenes
-}
+  // console.log(data);
+  return data.cumulativeGenes;
+};
 
-const SamplePatientDropdown = () => {
+const SamplePatientDropdown = () => {};
 
-}
+const GeneTextBox = ({ setGenes }) => {
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <TextField
+        label="Input genes"
+        multiline
+        rows="4"
+        value={text}
+        onChange={event => setText(event.target.value)}
+      />
+      <Button onClick={() => setGenes(textToGenes(text))}>Submit</Button>
+    </div>
+  );
+};
 
-const geneSearch = () => {
-  return []
-}
+const genesToText = genes => {
+  if (genes.length === 0) {
+    return "";
+  }
+
+  const [firstGene, ...restGenes] = genes;
+
+  return restGenes.reduce((str, gene) => `${str},${gene}`, firstGene);
+};
+
+const textToGenes = text => text.split(",").map(gene => gene.trim());
 
 const CumulativeGenePlot = () => {
   // TODO: add drop down to select sample/patient
   // TODO: add text box that parses and stores labels, then pass to query that will give you density plot, then parse results into plot
-  
-  const data = getCumulativeGenes(TEST_ID, GENES)
-  console.log(data)
+  const [genes, setGenes] = useState([]);
+
+  const data = getCumulativeGenes(TEST_ID, GENES);
+
   const density = data;
   const colorScale = datum => "#FFFFFF";
 
   return (
     <Grid container direction="row">
+      <Grid item>
+        <GeneTextBox genes={genes} setGenes={setGenes} />
+      </Grid>
       <Grid item>
         <XYFrame
           {...getFrameProps({
