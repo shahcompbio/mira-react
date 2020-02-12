9c9b77c1256d9aa5fcb4761a077b21c759ae5984
Douglas Abrams <abramsd@mskcc.org>
2020-02-12 13:13:02 -0500

    preliminary stuff

--- src/sandbox/CumulativeGenePlot.js
+++ src/sandbox/CumulativeGenePlot.js
@@ -8,10 +8,54 @@ import Correlation from "./Correlation";
 const TEST_DASHBOARDID = "SPECTRUM-OV-014_CD45P";
 const TEST_DASHBOARDTYPE = "patient";
 
+const TEST_ID = "SPECTRUM-OV-014_CD45P";
+const TEST_LABEL = { label: "celltype", type: "CELL" };
+const GENES = ["CD2", "CD4", "VIM", "CAV1", "PTPRC"]
+
+import gql from "graphql-tag";
+
+
+const QUERY_CUMULATIVE_GENES = gql`
+  query($dashboardID: String!, $genes: []!) {
+    cumulativeGenes(dashboardID: genes) {
+      x
+      y
+      value
+    }
+  }
+`;
+
+const getCumulativeGenes = (ID, GENES) => {
+  const { data, loading } = useQuery(QUERY_CUMULATIVE_GENES, {
+    variables: {
+      dashboardID: ID,
+      genes: GENES
+    }
+  });
+
+  if (loading && !data) {
+    return null;
+  }
+
+  return data
+}
+
+const SamplePatientDropdown = () => {
+
+}
+
+const geneSearch = () => {
+  genes = []
+  return genes
+}
+
 const CumulativeGenePlot = () => {
   // TODO: add drop down to select sample/patient
   // TODO: add text box that parses and stores labels, then pass to query that will give you density plot, then parse results into plot
-  const density = [];
+  
+  data = getCumulativeGenes(TEST_ID, GENES)
+  console.log(DATA)
+  const density = testData;
   const colorScale = datum => "#FFFFFF";
 
   return (
@@ -53,4 +97,4 @@ const getFrameProps = ({ data, label, highlightedGroup, colorScale }) => ({
   ]
 });
 
-export default () => null;
+export default CumulativeGenePlot;
