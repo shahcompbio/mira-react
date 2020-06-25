c259c55f17a04744177f42553024f7adb99a8e62
Leung <leungs1@slvicosspecvis1.mskcc.org>
2020-06-25 18:01:05 -0400

    Minor changes to match schema

--- src/Dashboard/getColors.js
+++ src/Dashboard/getColors.js
@@ -3,47 +3,40 @@ import {
   interpolateRainbow,
   interpolateViridis,
   interpolateOrRd,
-  schemeCategory10
+  schemeCategory10,
 } from "d3-scale-chromatic";
 
-export const getCelltypeColors = data => {
+export const getCelltypeColors = (data) => {
   const ordinalScale = getOrdinalScale(data);
   const toColorScale = scaleSequential(interpolateRainbow).domain([0, 1]);
-  return datum => toColorScale(ordinalScale(datum));
+  return (datum) => toColorScale(ordinalScale(datum));
 };
 
-export const getGeneColorScale = maxExpression => {
+export const getGeneColorScale = (maxExpression) => {
   const toColorScale = scaleSequential(interpolateViridis).domain([
     0,
-    maxExpression
+    maxExpression,
   ]);
 
-  return datum => toColorScale(datum);
+  return (datum) => toColorScale(datum);
 };
 
-const getOrdinalScale = data =>
+const getOrdinalScale = (data) =>
   scalePoint()
     .domain(data)
     .range([0, 0.9]);
 
 export default (label, data) => {
   if (label["isNum"]) {
-    if (label["type"] === "CELL") {
-      const toColorScale = scaleSequential(interpolateOrRd).domain([-0.2, 1]);
-      return datum => toColorScale(datum);
-    } else {
-      // IS GENE
-      const maxDataBucket = data[data.length - 1];
-
-      return getGeneColorScale(maxDataBucket);
-    }
+    // IS GENE
+    const maxDataBucket = data[data.length - 1];
+    return getGeneColorScale(maxDataBucket);
   } else {
-    if (label["type"] === "CELL") {
+    if (label["label"] === "cell_type") {
       return getCelltypeColors(data);
     } else {
-      // IS SAMPLE
       const toColorScale = scaleOrdinal(schemeCategory10).domain(data);
-      return datum => toColorScale(datum);
+      return (datum) => toColorScale(datum);
     }
   }
 };
